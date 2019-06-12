099d7fc0e58e6e4a5c64ef7f6b051d75b4767117
Michael Su <xmichael.su@gmail.com>
2019-06-12 11:01:51 -0700

    Implement List and Chip from material-ui

--- src/ChatUI.jsx
+++ src/ChatUI.jsx
@@ -2,11 +2,34 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles(theme => ({
   root: {
     margin: '50px',
     padding: theme.spacing(3, 2)
+  },
+  flexbox: {
+    display: 'flex'
+  },
+  topicsWindow: {
+    width: '30%',
+    height: '300px',
+    borderRight: '1px solid grey'
+  },
+  chatWindow: {
+    width: '70%',
+    height: '300px'
+  },
+  chatBox: {
+    width: '85%'
+  },
+  button: {
+    width: '15%'
   }
 }));
 
@@ -16,12 +39,35 @@ const ChatUI = () => {
   return (
     <div>
       <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
+        <Typography variant="h4" component="h4">
           Hello World
         </Typography>
-        <Typography component="p">
+        <Typography variant="h5" component="h5">
           This is a test for material-ui with react!
         </Typography>
+        <div className={classes.flexbox}>
+          <div className={classes.topicsWindow}>
+            <List>
+              {['topic'].map(topic => {
+                return (
+                  <ListItem key={topic} button>
+                    <ListItemText primary={topic} />
+                  </ListItem>
+                );
+              })}
+            </List>
+          </div>
+          <div className={classes.chatWindow}>
+            {[{ from: 'user', msg: 'hello' }].map(chat => {
+              return (
+                <div className={classes.flex}>
+                  <Chip label="Basic Chip" className={classes.chip} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className={classes.flexbox} />
       </Paper>
     </div>
   );
